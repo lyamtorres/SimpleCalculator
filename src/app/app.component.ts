8252b39922a4ba9458cf53bdca95b5efc26e67b7
lyamtorres <lyamtorres@hotmail.com>
2023-08-06 18:28:01 +0200

    basic functionnality done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-calculator';
+  displayValue: string = '0';
+  currentValue: number = 0;
+  operator: string | null = null;
+  previousValue: number = 0;
+
+  select(value: number): void {
+    this.displayValue = value.toString();
+    this.currentValue = value;
+  }
+
+  add(): void {
+    this.previousValue = this.currentValue;
+    this.operator = 'add';
+  }
+
+  subtract(): void {
+    this.previousValue = this.currentValue;
+    this.operator = 'subtract';
+  }
+
+  multiply(): void {
+    this.previousValue = this.currentValue;
+    this.operator = 'multiply';
+  }
+
+  divide(): void {
+    this.previousValue = this.currentValue;
+    this.operator = 'divide';
+  }
+
+  equals(): void {    
+      switch (this.operator) {
+        case 'add':
+          this.currentValue = this.previousValue + parseFloat(this.displayValue);
+          this.displayValue = this.currentValue.toString();
+          break;
+        case 'subtract':
+          this.currentValue = this.previousValue - parseFloat(this.displayValue);
+          this.displayValue = this.currentValue.toString();
+          break;
+        case 'multiply':
+          this.currentValue = this.previousValue * parseFloat(this.displayValue);
+          this.displayValue = this.currentValue.toString();
+          break;
+        case 'divide':
+          this.currentValue = this.previousValue / parseFloat(this.displayValue);
+          this.displayValue = this.currentValue.toString();
+          break;
+        default:
+          break;
+      }
+  }
 }
